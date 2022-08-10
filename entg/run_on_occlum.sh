5534250a3ad578c3cc5f99f854f71709a674559b
imlk <me@imlk.top>
2022-08-10 16:14:20 +0000

    entg: make entg-occlum to be able to run on occlum
    This commit makes changes to entg/run_on_occlum.sh and entg/occlum.yaml
    for running entg-occlum on occlum. Now entg/occlum.yaml should be able
    to collect all the dependent libraries.
    
    Now you can directly run `make demo` to start a four-party (enta_c --
    entg_c --<rats-tls>-- entg_s -- enta_s) test environment.

--- entg/run_on_occlum.sh
+++ entg/run_on_occlum.sh
@@ -5,9 +5,6 @@ BASE_DIR="$(realpath $(dirname $0))"
 
 pushd "$BASE_DIR"
 
-# compile
-occlum-cargo build --package entg
-
 # initialize occlum workspace
 rm -rf occlum_instance && mkdir occlum_instance && cd occlum_instance
 
@@ -16,8 +13,10 @@ occlum init && rm -rf image
 # enlarge .resource_limits.max_num_of_threads
 cp Occlum.json Occlum.json.bak && jq '.resource_limits.kernel_space_heap_size = "64MB" | .resource_limits.max_num_of_threads = 64' Occlum.json.bak > Occlum.json && rm Occlum.json.bak
 
+# TODO: Fix bug that copy file to `entg/occlum_instance/image/root/enclave-network-gateway/deps/rats-tls/build-occlum/src/librats_tls.so.0`, which is caused by RUNPATH of lib*.so
+# Note: set OCCLUM_LOG_LEVEL=trace to show more log of `copy_bom`
 copy_bom -f ../occlum.yaml --root image --include-dir /opt/occlum/etc/template
 
 occlum build
-occlum run /bin/entg $@
+occlum run /bin/entg-occlum $@
 popd
