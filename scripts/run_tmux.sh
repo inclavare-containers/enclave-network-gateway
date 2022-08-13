b2023899a977b02c7d6ed52434ea455c50dd6ade
imlk <me@imlk.top>
2022-08-13 13:54:12 +0000

    scripts: change make demo to launch echosvr demo

--- scripts/run_tmux.sh
+++ scripts/run_tmux.sh
@@ -8,7 +8,7 @@ bash -x "$(dirname $0)/setup_netns.sh"
 
 tmux kill-session -t eng 2>&- || true
 tmux new -d -s eng
-tmux split-window -p 66 \; split-window -d \; split-window -h \; select-pane -t 0 \; split-window -h
+tmux split-window -p 66 \; split-window -d \; split-window -h \; select-pane -t 0 \; split-window -h \; select-pane -t 4 \; split-window -h
 
 tmux send-keys -t eng:0.0 \
     "exec ip netns exec enta_c bash" ENTER \
@@ -33,8 +33,15 @@ tmux send-keys -t eng:0.3 \
         "$(dirname $0)/../entg/run_on_occlum.sh --entg-rats-tls --entg-listen 6979 --enta-rats-tls --enta-listen 6980" ENTER
 
 tmux send-keys -t eng:0.4 \
-    "sleep 9" ENTER \
-    "ip netns exec enta_c ping 192.168.0.254" ENTER
+    "exec ip netns exec enta_c bash" ENTER \
+        "sleep 9.5" ENTER \
+        "$(dirname $0)/../examples/echosvr/target/debug/echosvr client --host 1.1.1.1 --port 7" ENTER
+
+
+tmux send-keys -t eng:0.5 \
+    "exec ip netns exec enta_s bash" ENTER \
+        "sleep 9" ENTER \
+        "$(dirname $0)/../examples/echosvr/target/debug/echosvr server --host 0.0.0.0 --port 7" ENTER
 
 tmux a -t eng
 
